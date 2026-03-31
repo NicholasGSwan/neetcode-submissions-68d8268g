669d549ddda51d6170e173b24daebf2a1ece0c38
NicholasGSwan <30604585+NicholasGSwan@users.noreply.github.com>
2026-03-31 11:34:00 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.go
@@ -0,0 +1,16 @@
+func isAnagram(s string, t string) bool {
+    m := make(map[rune]int)
+
+    for _, v := range s {
+        m[v]++
+    }
+    for _, v := range t {
+        m[v]--
+    }
+    for _, v := range m {
+        if v != 0 {
+            return false
+        }
+    }
+    return true
+}
